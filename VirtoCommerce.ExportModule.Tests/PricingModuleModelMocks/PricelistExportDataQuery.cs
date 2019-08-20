4278f6f3ac02ea2ad9b114ed0e3492a1d51e51ed
Aleksandr Andreev <alivemenone@gmail.com>
2019-08-20 10:50:13 +0300

    move .test code from v3 to v2

---
+++ VirtoCommerce.ExportModule.Tests/PricingModuleModelMocks/PricelistExportDataQuery.cs
@@ -0,0 +1,14 @@
+using VirtoCommerce.Domain.Commerce.Model.Search;
+using VirtoCommerce.ExportModule.Core.Model;
+
+namespace VirtoCommerce.ExportModule.Tests.PricingModuleModelMocks
+{
+    public class PricelistExportDataQuery : ExportDataQuery
+    {
+
+    }
+
+    public class PricelistSearchCriteria : SearchCriteriaBase
+    {
+    }
+}
