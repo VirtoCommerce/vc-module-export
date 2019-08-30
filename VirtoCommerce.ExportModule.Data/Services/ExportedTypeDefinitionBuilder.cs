958ffd1ea138f8d422a7bc75d20e11dea9536dc4
Andrey Kozlov <yecli.github@gmail.com>
2019-08-30 19:08:14 +0500

    VP-362: ExportDataQuery type registration in Abstract type factory moved to ExportedTypeDefinitionBuilder;

--- VirtoCommerce.ExportModule.Data/Services/ExportedTypeDefinitionBuilder.cs
+++ VirtoCommerce.ExportModule.Data/Services/ExportedTypeDefinitionBuilder.cs
@@ -1,5 +1,6 @@
 using System;
 using VirtoCommerce.ExportModule.Core.Model;
+using VirtoCommerce.Platform.Core.Common;
 
 namespace VirtoCommerce.ExportModule.Data.Services
 {
@@ -27,6 +28,8 @@ namespace VirtoCommerce.ExportModule.Data.Services
         {
             var exportedType = typeof(TExportable);
             var dataQueryType = typeof(TDataQuery);
+
+            AbstractTypeFactory<ExportDataQuery>.RegisterType<TDataQuery>();
 
             return new ExportedTypeDefinitionBuilder(new ExportedTypeDefinition()
             {
