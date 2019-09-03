11265de8f9b45c89fae0315eff58c45f0e126b5b
Andrei Kolchanov <akak1977@gmail.com>
2019-09-03 15:24:52 +0500

    Added tests for complexExportPagedDataSources

---
+++ VirtoCommerce.ExportModule.Tests/ComplexExportPagedDataSourceTests/Mocks/ComplexSearchResult.cs
@@ -0,0 +1,10 @@
+using VirtoCommerce.Domain.Commerce.Model.Search;
+using VirtoCommerce.ExportModule.Core.Model;
+using VirtoCommerce.Platform.Core.Common;
+
+namespace VirtoCommerce.ExportModule.Tests.ComplexExportPagedDataSourceTests.Mocks
+{
+    public class ComplexSearchResult : GenericSearchResult<IExportable>
+    {
+    }
+}
