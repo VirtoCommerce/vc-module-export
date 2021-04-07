0605e7da419741bb1b9920a65417a8945aeac1cd
Andrei Kolchanov <akak1977@gmail.com>
2021-04-07 16:48:39 +0500

    PT-464: Use "default" encoding as default encoding value in CsvProviderConfiguration instead of "utf-8"

--- src/VirtoCommerce.ExportModule.CsvProvider/CsvProviderConfiguration.cs
+++ src/VirtoCommerce.ExportModule.CsvProvider/CsvProviderConfiguration.cs
@@ -16,6 +16,6 @@ namespace VirtoCommerce.ExportModule.CsvProvider
         /// Name of the codepage should be used.  
         /// Full list of codepages can be found at https://docs.microsoft.com/en-US/dotnet/api/system.text.encoding?view=netcore-3.1
         /// </summary>
-        public string Encoding { get; set; } = "utf-8";
+        public string Encoding { get; set; } = System.Text.Encoding.Default.EncodingName;
     }
 }
