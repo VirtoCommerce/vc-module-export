a729dca847def26c1dd521d06a9a1765fc3409b3
Andrei Kolchanov <akak1977@gmail.com>
2021-04-07 11:35:32 +0500

    PT-464: Remove smells

--- src/VirtoCommerce.ExportModule.Core/Model/IExportProviderConfiguration.cs
+++ src/VirtoCommerce.ExportModule.Core/Model/IExportProviderConfiguration.cs
@@ -2,7 +2,9 @@ namespace VirtoCommerce.ExportModule.Core.Model
 {
     public interface IExportProviderConfiguration
     {
+        /// <summary>
+        /// Type discriminator to instantiate proper descendant (a.e. thru the universal PolymorphJsonConverter)
+        /// </summary>
         public string Type { get; set; }
-
     }
 }
