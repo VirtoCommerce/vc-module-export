4e971393079a4cc65bdc72ba69c5374771f77791
Andrei Kolchanov <akak1977@gmail.com>
2021-05-21 16:18:09 +0500

    Fix CsvProviderConfiguration default delimiter

--- src/VirtoCommerce.ExportModule.Web/Scripts/blades/export-settings.js
+++ src/VirtoCommerce.ExportModule.Web/Scripts/blades/export-settings.js
@@ -127,13 +127,6 @@ angular.module('virtoCommerce.exportModule')
                 return;
             }
 
-            var csvProviderConfig = {
-                type: 'CsvProviderConfiguration',
-                delimiter: '^'
-            }
-
-            blade.exportDataRequest.providerConfig = csvProviderConfig;
-
             blade.exportDataRequest.providerName = blade.selectedProvider.id;
             delete blade.exportDataRequest.dataQuery.skip;
             delete blade.exportDataRequest.dataQuery.take;
